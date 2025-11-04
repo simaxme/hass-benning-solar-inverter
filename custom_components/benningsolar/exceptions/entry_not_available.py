0f7ee73944c641c61f0ab17630ff2f37b6e4bb77
simaxme <contact@simax.me>
2025-11-04 17:42:13 +0100

    Added documentation

--- custom_components/benningsolar/exceptions/entry_not_available.py
+++ custom_components/benningsolar/exceptions/entry_not_available.py
@@ -2,4 +2,4 @@
 from homeassistant.exceptions import HomeAssistantError
 
 class EntryNotAvailable(HomeAssistantError):
-    """Error to indicate we cannot connect."""
+    """Error to indicate that a specified entry from the inverter is not available."""
